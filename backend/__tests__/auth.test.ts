559aeae6f7fa69deccc2f530bb981d873aae84e6
Sergej Herbert <herbert.sergej@gmail.com>
2020-02-23 09:36:03 +0100

    updated auth tests

--- backend/__tests__/auth.test.ts
+++ backend/__tests__/auth.test.ts
@@ -5,32 +5,36 @@ describe("authentication", () => {
     await server.ready();
   });
 
-  it("returns 401 for unauthenticated requests", async () => {
-    const response = await server.inject({ method: "GET", url: "/" });
-    expect(response.statusCode).toEqual(401);
-  });
+  const protectedRoutes = ["/api/clients", "/api/clients/not_existing_id"];
 
-  it("returns 401 for wrong credentials", async () => {
-    const encoded_credentials = Buffer.from("not_a_user:not_a_pass").toString("base64");
-    const response = await server.inject({
-      method: "GET",
-      headers: { Authorization: `Basic ${encoded_credentials}` },
-      url: "/",
+  for (const url of protectedRoutes) {
+    it("returns 401 for unauthenticated requests", async () => {
+      const response = await server.inject({ method: "GET", url });
+      expect(response.statusCode).toEqual(401);
     });
-    expect(response.statusCode).toEqual(401);
-  });
 
-  it("returns 200 for authenticated requests", async () => {
-    const credentials = ["test_user_1:test_pass_1", "test_user_2:test_pass_2"];
-
-    for (const credential of credentials) {
-      const encoded_credentials = Buffer.from(credential).toString("base64");
+    it("returns 401 for wrong credentials", async () => {
+      const encoded_credentials = Buffer.from("not_a_user:not_a_pass").toString("base64");
       const response = await server.inject({
         method: "GET",
         headers: { Authorization: `Basic ${encoded_credentials}` },
-        url: "/",
+        url,
       });
-      expect(response.statusCode).toEqual(200);
-    }
-  });
+      expect(response.statusCode).toEqual(401);
+    });
+
+    it("returns 200 for authenticated requests", async () => {
+      const credentials = ["test_user_1:test_pass_1", "test_user_2:test_pass_2"];
+
+      for (const credential of credentials) {
+        const encoded_credentials = Buffer.from(credential).toString("base64");
+        const response = await server.inject({
+          method: "GET",
+          headers: { Authorization: `Basic ${encoded_credentials}` },
+          url,
+        });
+        expect(response.statusCode).not.toEqual(401);
+      }
+    });
+  }
 });
--- backend/server.ts
+++ backend/server.ts
@@ -2,13 +2,11 @@ import * as fastify from "fastify";
 import * as fastifyBasicAuth from "fastify-basic-auth";
 
 import { validate } from "@backend/auth";
-import dummyRoutes from "@backend/routes/dummy";
 import clientRoutes from "@backend/routes/clients";
 
 const server = fastify();
 
 server.register(fastifyBasicAuth, { validate, authenticate: { realm: "kfz-werkstatt" } });
-server.register(dummyRoutes);
 server.register(clientRoutes);
 
 server.after(() => {
